0f39c7ccf84e68aa30ec87fe4b86df11481be349
Prajwal H C <68982347+prajwalhc-18@users.noreply.github.com>
2024-07-10 12:31:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function handleSearch(event) {
+  event.preventDefault(); // Prevent the default form submission
+  const searchInput = document
+    .getElementById("searchInput")
+    .value.trim()
+    .toLowerCase();
+
+  if (searchInput === "home") {
+    window.location.href = "index.html";
+  } else if (searchInput === "about") {
+    window.location.href = "about.html";
+  } else if (searchInput === "contact") {
+    window.location.href = "contact.html";
+  } else {
+    alert("Page not found"); // Handle invalid search terms
+  }
+}
