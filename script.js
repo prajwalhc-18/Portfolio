bd6bada060c1868cc4e7d4bb5992a66bf6a5ad76
prajwalhc-18 <prajwalhc1806@gmail.com>
2024-07-11 00:43:04 +0530

    updated - messaging feature

--- script.js
+++ script.js
@@ -16,3 +16,34 @@ function handleSearch(event) {
   }
 }
 
+////////////////////////////////////////////////////////////
+
+// document
+//   .getElementById("message-section")
+//   .addEventListener("submit", function (event) {
+//     event.preventDefault();
+
+//     let params = {
+//       to_name: "Rahul Yadav", // Replace with actual recipient name
+//       from_name: document.getElementById("name").value,
+//       email_id: document.getElementById("email").value,
+//       service: document.getElementById("services").value,
+//       phone_number: document.getElementById('phone').value,
+//       message: document.getElementById("message").value,
+//     };
+
+//     emailjs.send("service_jaztsda", "template_ou0e6jd", params).then(
+//       function (response) {
+//         console.log("SUCCESS!", response.status, response.text);
+//         alert("Your message has been sent!");
+//         document.getElementById("message-section").reset(); // Reset the form
+//         setTimeout(function () {
+//           location.reload(); // Refresh the page after a short delay
+//         }, 500); // Adjust the delay time as needed
+//       },
+//       function (error) {
+//         console.log("FAILED...", error);
+//         alert("Failed to send your message.");
+//       }
+//     );
+//   });
